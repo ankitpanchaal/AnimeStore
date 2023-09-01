027cf4f30208dd1e226d547e660e7b683ac26c68
ankitpanchaal <ankit0panchal@gmail.com>
2023-09-01 10:39:28 +0530

    working

--- src/components/ProductCard2.jsx
+++ src/components/ProductCard2.jsx
@@ -7,8 +7,8 @@ const ProductCard2 = ({ item }) => {
     return (
         <Link href={`/singleProduct/${slug}`}>
             <div className="p-2 rounded-md bg-[#FF676D20] lg:p-3 cursor-pointer transform transition-all duration-300 hover:scale-105">
-                <div className="items-center max-w-[150px]">
-                    <img src={imageUrl(item.image[0])} alt="" className="h-[150px] w-[150px] rounded-md -z-10 object-contain" />
+                <div className="items-center max-w-[150px] lg:max-w-[250px]">
+                    <img src={imageUrl(item.image[0])} alt="" className="h-[150px] w-[150px] lg:h-[250px] lg:w-[250px] rounded-md -z-10 object-contain" />
                     <div className="mt-2 px-2">
                         <div className="font-semibold text-sm text-black line-clamp-2">{item.name}</div>
                         <div className="font-medium text-[10px] text-[#FF676D]">Price: ₹  {item.price}</div>
